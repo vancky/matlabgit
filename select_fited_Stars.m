5caa8f0cff160721d79b91288814135fa168751f
vancky <vanckyli@gmail.com>
2015-05-07 15:53:04 +0800

    add new select ways to disable those same stars

--- select_fited_Stars.m
+++ select_fited_Stars.m
@@ -1,8 +1,15 @@
-function [ outdata ] = select_fited_Stars(indata,lowbound)
+function [ outdata ] = select_fited_Stars(indata,lowbound,xmax,ymax)
 %Select those well fited stars
 % delete those adjust<lowbound
-index=indata(:,end)>lowbound;
-index=find(index)';
-outdata=indata(:,index);
+indata=sortrows(indata,1);
+index=find(indata(:,end)>lowbound...  % how well the fit does
+        &indata(:,1)<xmax...            % something may wrong 
+        &indata(:,2)<ymax...     
+        &indata(:,1)>0&indata(:,2)>0 ... 
+        &abs([diff(indata(:,2));3])>2)'; % for some the same one 
+
+%index=indata(:,end)>lowbound;
+%index=find(index)';
+outdata=indata(index,:);
 end
 
